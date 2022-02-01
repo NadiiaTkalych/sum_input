8267315e64e3af6ba7f0b847d433262465568a02
NadiiaTkalych <93153046+NadiiaTkalych@users.noreply.github.com>
2022-02-01 19:04:05 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const inputValue1 = Number(document.getElemetnById('input-1').value),
+  inputValue2 = Number(document.getElemetnById('input-2').value),
+  inputValue3 = Number(document.getElemetnById('input-3').value),
+  inputResult = document.getElemetnById('input-result'),  
+  btn = document.getElemetnById('btn');
+
+let result;
+
+const countingInpun = () => {
+   result = inputValue1 + inputValue2 + inputValue3
+}
+
+btn.addEventListener('click', countingInpun());
+
+inputResult.innerHTML = result;
