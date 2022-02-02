36ebd6090a99d521f6669c6f66587126540e7543
NadiiaTkalych <93153046+NadiiaTkalych@users.noreply.github.com>
2022-02-02 15:08:53 +0200

    fix old code

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const inputValue1 = Number(document.getElemetnById('input-1').value),
-  inputValue2 = Number(document.getElemetnById('input-2').value),
-  inputValue3 = Number(document.getElemetnById('input-3').value),
-  inputResult = document.getElemetnById('input-result'),  
-  btn = document.getElemetnById('btn');
+ const inputValue1 = document.getElementById('input-1'),
+   inputValue2 = document.getElementById('input-2'),
+   inputValue3 = document.getElementById('input-3'),
+   inputResult = document.getElementById('input-result'),  
+   btn = document.getElementById('btn');
 
-let result;
+ let result;
+ const strToNum = str => str.value ? parseInt(str.value) : 0
 
-const sumInpun = () => {
-   result = inputValue1 + inputValue2 + inputValue3
-}
+ btn.addEventListener('click', () => {
+   result = strToNum(inputValue1) + strToNum(inputValue2) + strToNum(inputValue3)
+   inputResult.value = result;
+ });
 
-btn.addEventListener('click', sumInpun());
+// const inputValue1 = Number(document.getElemetnById('input-1').value),
+//   inputValue2 = Number(document.getElemetnById('input-2').value),
+//   inputValue3 = Number(document.getElemetnById('input-3').value),
+//   inputResult = document.getElemetnById('input-result'),  
+//   btn = document.getElemetnById('btn');
 
-inputResult.innerHTML = result;
-// или это
-//inputResult.innerHTML = sumInpun();
+// let result;
+
+// const sumInpun = () => {
+//    result = inputValue1 + inputValue2 + inputValue3
+// }
+
+// btn.addEventListener('click', sumInpun());
+
+// inputResult.innerHTML = result;
+// // или это
+// //inputResult.innerHTML = sumInpun();
